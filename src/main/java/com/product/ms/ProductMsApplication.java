31b1c3f147704191a82b750d0a82f63a126de9ff
Elliot Escovitch <dev.escovitch.ri@gmail.com>
2024-08-16 23:55:37 -0500

    add database connection

--- src/main/java/com/product/ms/ProductMsApplication.java
+++ src/main/java/com/product/ms/ProductMsApplication.java
@@ -1,13 +1,19 @@
 package com.product.ms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductMsApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(ProductMsApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProductMsApplication.class, args);
+		logger.info("Welcome to Product Microservice!");
 	}
 
 }
